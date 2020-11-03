b1bc6e92e91143c89538a7e2113f234b20346127
karazKOS <ykaraiscos@yahoo.com>
2020-11-02 22:04:12 -0800

    fixed syntax errors on connection and orm

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,13 @@ var mysql = require("mysql");
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Phoenix11!",
-  database: "burgers_db",
-});
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Phoenix11!",
+    database: "burgers_db",
+  });
+}
 connection.connect();
 
 module.exports = connection;
